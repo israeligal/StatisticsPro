a1644e0e3e1789360fb7e7d3b88e8f1cea6d037f
shahar3 <shahar061@gmail.com>
2017-11-01 10:59:52 +0200

    Added place picker and changed the UI of the list items

--- app/src/main/java/com/example/shaha/mepo/EventsListAdapter.java
+++ app/src/main/java/com/example/shaha/mepo/EventsListAdapter.java
@@ -9,6 +9,9 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -31,9 +34,32 @@ public class EventsListAdapter extends ArrayAdapter<MepoEvent>{
 
         MepoEvent currentEvent = getItem(position);
 
-        TextView eventTitleTextView = eventListItemView.findViewById(R.id.event_title_text_view);
+        TextView eventTitleTextView = (TextView)eventListItemView.findViewById(R.id.event_title_text_view);
         eventTitleTextView.setText(currentEvent.getEventName());
 
+        TextView eventDateTextView = (TextView)eventListItemView.findViewById(R.id.list_item_event_date);
+        eventDateTextView.setText(getDate(currentEvent.getStartTime()));
+
+        TextView eventTimeTextView = (TextView)eventListItemView.findViewById(R.id.list_item_event_time);
+        eventTimeTextView.setText(getStartTime(currentEvent.getStartTime())+"-"+getEndTime(currentEvent.getEndTime()));
+
         return eventListItemView;
+    }
+
+    private String getStartTime(Date date) {
+        return new SimpleDateFormat("HH:mm").format(date);
+    }
+
+    private String getEndTime(Date date) {
+        return new SimpleDateFormat("HH:mm").format(date);
+    }
+
+    /**
+     * Get the event start date and represent it as a string
+     * @param startTime
+     * @return
+     */
+    private String getDate(Date startTime) {
+        return DateFormat.getDateInstance(DateFormat.SHORT).format(startTime);
     }
 }
